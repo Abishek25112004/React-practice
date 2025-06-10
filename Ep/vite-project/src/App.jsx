7c56d7b0ecf0898ef01fc48e2fa99d106b3b7df0
Abishek M <167514119+Abishek25112004@users.noreply.github.com>
2025-06-10 13:46:08 +0530

    deleting functional and class files

--- Ep/vite-project/src/App.jsx
+++ Ep/vite-project/src/App.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ClassBasedComponent from './classComponents';
+import FunctionalComponent from './functionalComponent';
 
 
 
 
 function App() {
-  const arr = ["a","b","c"];
-  
   return (
-    <>
-    <Abi ans={arr}/>
-    </>
+  
+      
+        <ClassBasedComponent />
+    
+    
     
   )
  }
