c8ed00ab28ec568768008e22b89e00170aa685dd
Abishek M <167514119+Abishek25112004@users.noreply.github.com>
2025-06-12 19:41:07 +0530

    learning UseRef

--- Ep/vite-project/src/Hook-UseRef.jsx
+++ Ep/vite-project/src/Hook-UseRef.jsx
@@ -1,16 +1,29 @@
-import {useState} from 'react'
+import {useState , useRef, useEffect} from 'react'
 
 function HookUseRef() {
      
     let [input,setInput] = useState("")
 
+    let inputRef = useRef();
+    let ans;
+
 console.log(input)
+
+let display=()=>{
+       ans  = inputRef.current.value
+       setInput(ans)
+       console.log(ans)
+     
+}
+
+
+
 
   return (
     <div>
-     <input style={{border:"3px solid black"}} type="text"  value={input} onChange={(e)=>{setInput(e.target.value)}}/>
+     <input style={{border:"3px solid black"}} type="text"  ref={inputRef}  />
       <h2>My name is : {input}</h2>
-      <button>show</button>
+      <button onClick={display}>show</button>
     </div>
   )
 }
